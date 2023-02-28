dab7e650b289db07560b59d5b3fe238b7cbd3aa9
maganijnr <maganijoshua25@gmail.com>
2023-02-28 16:36:35 +0100

    upload page

--- src/pages/CreatePage/index.tsx
+++ src/pages/CreatePage/index.tsx
@@ -1,66 +1,105 @@
-import React from "react";
+import { useState, useRef } from "react";
+import "./index.scss";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { convertToBase64 } from "../../helpers/convertImageToBase64";
+import { uploadPost, uploadPostImage } from "../../store/actions/postActions";
+import { ThreeCircles } from "react-loader-spinner";
+import { useAppSelector } from "../../store/hooks/app";
 
 const CreatePage = () => {
-	// const convertToBase64 = (file: any) => {
-	// 	return new Promise((resolve, reject) => {
-	// 		const fileReader = new FileReader();
+	const [selectedFile, setSelectedFile] = useState("");
+	const [imageLoading, setImageLoading] = useState<boolean>(false);
+	const [message, setMessage] = useState<string>("");
+	const [loading, setLoading] = useState<boolean>(false);
+	const auth = useAppSelector((state) => state.auth);
 
-	// 		fileReader.readAsDataURL(file);
+	const { token } = auth;
 
-	// 		fileReader.onload = () => resolve(fileReader.result);
+	const imageRef = useRef<HTMLInputElement>();
+	const handleImageSubmit = async (event: any) => {
+		const file = event.target.files[0];
+		const base64 = await convertToBase64(file);
 
-	// 		fileReader.onerror = (error) => reject(error);
-	// 	});
-	// };
+		setImageLoading(true);
 
-	// const uploadImage = async (event: any) => {
-	// 	const file = event.target.files[0];
-	// 	const base64 = await convertToBase64(file);
-	// 	setLoading(true);
+		try {
+			const response = await uploadPostImage(base64);
+			setSelectedFile(response);
+			setImageLoading(false);
+		} catch (error) {
+			console.log(error);
+			setImageLoading(false);
+		}
+	};
 
-	// 	axios
-	// 		.post(`http://localhost:8000/api/upload-post`, {
-	// 			image: base64,
-	// 		})
-	// 		.then((res) => {
-	// 			setImageUrl(res.data);
-	// 			setLoading(false);
-	// 		})
-	// 		.catch((err) => console.log(err));
-	// };
+	const handleSubmit = async (e: { preventDefault: () => void }) => {
+		e.preventDefault();
+		setLoading(true);
+		if (!token) {
+			alert("Not logged in");
+			return setLoading(false);
+		}
 
-	// const uploadPost = async () => {
-	// 	const config = {
-	// 		headers: {
-	// 			Authorization: `Bearer ${token}`,
-	// 			"Content-Type": "application/json",
-	// 		},
-	// 	};
+		if ((selectedFile || message) && token) {
+			await uploadPost(message, selectedFile, token);
+			alert("Uploaded successfully");
+			setLoading(false);
+		}
+	};
 
-	// 	const formData = {
-	// 		message,
-	// 		imageUrl,
-	// 	};
-
-	// 	setLoading(true);
-
-	// 	try {
-	// 		const response = await axios.post(
-	// 			`http://localhost:8000/api/posts`,
-	// 			formData,
-	// 			config
-	// 		);
-
-	// 		console.log(response);
-	// 		setLoading(false);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 		setLoading(false);
-	// 	}
-	// };
 	return (
 		<div className="page_container">
-			<h2>Create page</h2>
+			<h2>Create a post</h2>
+			<div className="form_body">
+				<form className="form" onSubmit={handleSubmit}>
+					<div
+						className="image_div"
+						// @ts-ignore
+						onClick={() => imageRef.current?.click()}
+					>
+						<input
+							// @ts-ignore
+							ref={imageRef}
+							type="file"
+							accept="image/png, image/jpeg, image/jpg, image/svg+xml, .blend, .obj"
+							name="profileImage"
+							id="profileImage"
+							onChange={handleImageSubmit}
+							hidden
+						/>
+						{imageLoading && (
+							<div className="image_loader">
+								<ThreeCircles color="#fff" width={20} height={20} />
+							</div>
+						)}
+						{selectedFile ? (
+							<img src={selectedFile} alt="selected image" />
+						) : (
+							<div className="icon_div">
+								<AiOutlineCloudUpload fontSize={30} />
+								<p>Upload Image</p>
+							</div>
+						)}
+					</div>
+					<div className="input_div">
+						<label htmlFor="message">Message</label>
+						<textarea
+							className="text_area"
+							name="message"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+							placeholder="What's on your mind?"
+						></textarea>
+					</div>
+					<button>
+						{loading ? (
+							<ThreeCircles color="#fff" width={20} height={20} />
+						) : (
+							"Upload"
+						)}
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 };
--- src/store/actions/postActions.ts
+++ src/store/actions/postActions.ts
@@ -50,3 +50,42 @@ export const likePost = async (postId: string, token: string) => {
 		return { error: msg };
 	}
 };
+
+export const uploadPostImage = async (image: any) => {
+	try {
+		const result = await axios.post(`${baseUrl}/api/upload-post`, {
+			image: image,
+		});
+		return result.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const uploadPost = async (
+	message: string,
+	imageUrl: string,
+	token: string
+) => {
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json",
+		},
+	};
+
+	const formData = {
+		message,
+		imageUrl,
+	};
+
+	try {
+		const response = await axios.post(
+			`${baseUrl}/api/posts`,
+			formData,
+			config
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
